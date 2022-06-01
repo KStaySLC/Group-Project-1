41760750b65e0c1e3565de597a39723189a0c3e2
Grant Register <register.grant@gmail.com>
2022-05-31 21:43:49 -0600

    the quiz actually works now

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-    function showQuestions(questions, quizContainer) {
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
         var output = [];
         var answers;
-
+    
+        // for each question...
         for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
             answers = [];
-
-            for(letter in questions[i].answers) {
-                
+    
+            // for each available answer to this question...
+            for(letter in questions[i].answers){
+    
+                // ...add an html radio button
                 answers.push(
                     '<label>'
                         + '<input type="radio" name="question'+i+'" value="'+letter+'">'
@@ -16,73 +23,101 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
                     + '</label>'
                 );
             }
+    
+            // add this question and its answers to the output
             output.push(
                 '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div'
+                + '<div class="answers">' + answers.join('') + '</div>'
             );
         }
+    
+        // finally combine our output list into one string of html and put it on the page
         quizContainer.innerHTML = output.join('');
     }
-showQuestions(questions, quizContainer);
+    // show the questions
+	showQuestions(questions, quizContainer);
 
-function showResults(questions, quizContainer, resultsContainer){
-    	// gather answer containers from our quiz
-	var answerContainers = quizContainer.querySelectorAll('.answers');
+	function showResults(questions, quizContainer, resultsContainer){
 	
-	// keep track of user's answers
-	var userAnswer = '';
-	var numCorrect = 0;
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+    
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+    
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
 	
-	// for each question...
-	for(var i=0; i<questions.length; i++){
 
-		// find selected answer
-		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-
-}
-
-showQuestions(questions, quizContainer);
-
-submitButton.onclick = function() {
-    showResults(questions, quizContainer, resultsContainer);
-}
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
 }
 
 var myQuestions = [
+	{
+		question: "What Size Dog Are We Looking For Here",
+		answers: {
+			a: 'Small',
+			b: 'Medium',
+			c: 'Large',
+            d: 'Enormous, like a horse'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "You Got Them Allergies?",
+		answers: {
+			a: 'Yup',
+			b: 'Nope',
+		},
+		correctAnswer: 'b'
+	},
     {
-        question: "What Size Dog Are We Looking For Here?",
-        answers: {
-            a: 'small',
-            b: 'medium',
-            c: 'large',
-            d: 'enormous....like almost a horse'
-        },
-    },
+		question: "What About Temperament?",
+		answers: {
+			a: 'Calm',
+			b: 'Protective',
+            c: 'Energetic',
+            d: 'Timid'
+		},
+		correctAnswer: 'c'
+	},
     {
-        question: "You Got Them Allergies? Don't Want A Shedding Dog?",
-        answers: {
-            a: 'yup',
-            b: 'nope',
-        },
-    },
-    {
-        question: "What About Temperament?",
-        answers: {
-            a: 'calm',
-            b: 'protective',
-            c: 'energetic',
-            d: 'social'
-        },
-    },
-    {
-        question: "Got Money To Burn?",
-        answers: {
-            a: 'of course not',
-            b: 'rich af',
-            c: 'cant put a price on love',
-        }
-    }
-]}
+		question: "Got Money To Burn?",
+		answers: {
+			a: 'Of Course Not',
+			b: 'Rich AF',
+            c: 'Love Matters More Than Money'
+		},
+		correctAnswer: 'a'
+	}
+];
 
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
