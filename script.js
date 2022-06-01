65d6ec1acd3533f9224a3ba69f2b95ebc7e0b891
Grant Register <register.grant@gmail.com>
2022-05-31 21:10:11 -0600

    tried to make a javascript quiz

--- script.js
+++ script.js
@@ -1,107 +1,91 @@
-var timer = document.getElementById('header1');
-var sec = 45;
-var timerID = setInterval(1000);
-function countDownTimer() {
-    if (sec == -1) {
-        clearTimeout(timerID);
-        // “DO Something this is where the question switches”
-    } else {
-        timer.innerHTML = sec + " seconds remaining!";
-        sec--;
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+    function showQuestions(questions, quizContainer) {
+        var output = [];
+        var answers;
+
+        for(var i=0; i<questions.length; i++){
+            answers = [];
+
+            for(letter in questions[i].answers) {
+                
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div'
+            );
+        }
+        quizContainer.innerHTML = output.join('');
     }
-}
-var startButton = document.getElementById("start");
-startButton.addEventListener("click", startTimer, countDownTimer);
-var questionsBox = document.getElementById("question-container");
-var questionRandom =
-function startTimer(quiz) {
-    startButton.classList.add("hide");
-    questionRandom = questions.sort(() => Math.random() - .5);
-    currentQuestion = 0
-    questionsBox.classList.remove("hide");
-    nextQuestion();
+showQuestions(questions, quizContainer);
+
+function showResults(questions, quizContainer, resultsContainer){
+    	// gather answer containers from our quiz
+	var answerContainers = quizContainer.querySelectorAll('.answers');
+	
+	// keep track of user's answers
+	var userAnswer = '';
+	var numCorrect = 0;
+	
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+
+		// find selected answer
+		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+
 }
 
-var germanShepard = {
-    size: 
-    temperment
-    price
-    hypoallergenic
+showQuestions(questions, quizContainer);
+
+submitButton.onclick = function() {
+    showResults(questions, quizContainer, resultsContainer);
+}
 }
 
-var labrador = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
+var myQuestions = [
+    {
+        question: "What Size Dog Are We Looking For Here?",
+        answers: {
+            a: 'small',
+            b: 'medium',
+            c: 'large',
+            d: 'enormous....like almost a horse'
+        },
+    },
+    {
+        question: "You Got Them Allergies? Don't Want A Shedding Dog?",
+        answers: {
+            a: 'yup',
+            b: 'nope',
+        },
+    },
+    {
+        question: "What About Temperament?",
+        answers: {
+            a: 'calm',
+            b: 'protective',
+            c: 'energetic',
+            d: 'social'
+        },
+    },
+    {
+        question: "Got Money To Burn?",
+        answers: {
+            a: 'of course not',
+            b: 'rich af',
+            c: 'cant put a price on love',
+        }
+    }
+]}
 
-var aussieDoodle = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
-var samoyed = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
-
-var terrier = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
-
-var shihTzu = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
-
-var aussieShepard = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
-
-var borderCollie = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
-
-var bichonFrise = [
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-]
-
-var toyPoodle = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
-
-var maltese = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
-
-var pomeranian = {
-    size: 
-    temperment:
-    price:
-    hypoallergenic:
-}
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
